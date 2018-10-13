e332794a2620561a0efc270637419fb0d64d585b
luki446 <luki446@gmail.com>
2018-10-13 02:21:16 +0200

    added readme and tests

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Example benchmarks in Rust  
+Simple benchmarks on fibonacci sequence in Rust made for learnign purposes    
+## Usage  
+To use you must have installed Rust from [here](https://www.rust-lang.org/)  
+Then run this command in root directory  
+```
+cargo bench  
+```
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate test;
 use self::test::Bencher;
 use std::option::Option;
 
-const COUNT: usize = 25;
+const COUNT: usize = 30;
 
 fn fibonacci_recursive(n: usize) -> u64
 {
@@ -20,7 +20,7 @@ fn fibonacci_recursive(n: usize) -> u64
 
 fn fibonacci_better(n: usize) -> u64
 {
-    let mut curr: u64 = 1;
+    let mut curr: u64 = 0;
     let mut next: u64 = 1;
     for _ in 1..=n
     {
@@ -45,10 +45,11 @@ impl Iterator for Fibonacci
     fn next(&mut self) -> Option<u64>
     {
         let new_next = self.curr + self.next;
+        let res = Some(self.curr);
         self.curr = self.next;
         self.next = new_next;
 
-        Some(self.curr)
+        res
     }
 }
 
@@ -56,9 +57,27 @@ fn fibonacci() -> Fibonacci
 {
     Fibonacci
     {
-        curr : 1,
+        curr : 0,
         next : 1
     }
+}
+
+#[test]
+fn recursive_test()
+{
+    assert_eq!(5702887, fibonacci_recursive(34))
+}
+
+#[test]
+fn better_test()
+{
+    assert_eq!(5702887, fibonacci_better(34))
+}
+
+#[test]
+fn iterator_test_test()
+{
+    assert_eq!(5702887, fibonacci().nth(34).unwrap())
 }
 
 #[bench]
